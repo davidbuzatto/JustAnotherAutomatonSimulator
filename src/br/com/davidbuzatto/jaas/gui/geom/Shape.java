0fd48bd0320e808af2b9a5e1c6b20f9e76050c18
David Buzatto <davidbuzatto@gmail.com>
2020-11-22 12:17:20 -0300

    More GUI tweeks and regression of JDK version to 1.8 (Java 8).

--- src/br/com/davidbuzatto/jaas/gui/geom/Shape.java
+++ src/br/com/davidbuzatto/jaas/gui/geom/Shape.java
@@ -17,7 +17,6 @@ import java.io.Serializable;
 public abstract class Shape implements Serializable {
     
     private static final long serialVersionUID = Constants.SERIAL_VERSION;
-    
     private static int idCount;
     private int id;
     
